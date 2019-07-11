82c414cf6bf2dd4ae871a1557aaa9cb78baa6ece
Sahar Hagbi <sahar.hagbi@hp.com>
2019-07-11 17:39:56 +0300

    Initial commit with main activity and its child.

--- app/src/main/java/com/example/getdown/MainActivity.java
+++ app/src/main/java/com/example/getdown/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.getdown;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.getdown.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void begin(View view){
+        Intent intent = new Intent(this, ChooseLocation.class);
+        startActivity(intent);
+    }
+
 }
