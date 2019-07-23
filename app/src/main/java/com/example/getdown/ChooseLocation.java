78af2d1e151e5aebd6fd84258994a1d7a496f8e5
Sahar Hagbi <sahar.hagbi@hp.com>
2019-07-23 14:30:22 +0300

    Added Location listener - fixed

---
+++ app/src/main/java/com/example/getdown/ChooseLocation.java
@@ -0,0 +1,48 @@
+package com.example.getdown;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ChooseLocation extends AppCompatActivity {
+
+    LocationManager mLocationManager;
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(final Location location) {
+            TextView textView = findViewById(R.id.textView_location);
+            textView.setText(location.getLongitude() + ", " + location.getLatitude());
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_location);
+
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10,
+                5, mLocationListener);
+    }
+}
