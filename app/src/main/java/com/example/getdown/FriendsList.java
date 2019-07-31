54f3c3dc00ecb40065758952d7338b32fbfc42bf
Sahar Hagbi <sahsahar@gmail.com>
2019-07-31 22:19:44 +0300

    Added an add contact form. Initial activity map.

--- app/src/main/java/com/example/getdown/FriendsList.java
+++ app/src/main/java/com/example/getdown/FriendsList.java
@@ -2,6 +2,7 @@ package com.example.getdown;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
@@ -40,7 +41,8 @@ public class FriendsList extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO
+                Intent intent = new Intent(FriendsList.this,AddFriendForm.class);
+                FriendsList.super.startActivity(intent);
             }
         });
 
---
+++ app/src/main/java/com/example/getdown/MapActivity.java
@@ -0,0 +1,16 @@
+package com.example.getdown;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MapActivity extends AppCompatActivity {
+
+    private static final String TAG = "MapActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
